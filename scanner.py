c1e8182e2574a4822277331ec017fbfdf6525d19
jacob16c <jayleeyong05@gmail.com>
2026-09-16 16:21:38 +0800

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,18 +1,2 @@
-import requests
-import os
-
-TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
-
-print("Bot Token Found:", TOKEN is not None)
-print("Chat ID Found:", CHAT_ID is not None)
-
-requests.post(
-    f"https://api.telegram.org/bot{TOKEN}/sendMessage",
-    data={
-        "chat_id": CHAT_ID,
-        "text": "✅ GitHub Action Test Successful"
-    }
-)
-
-print("Success")
+print("HELLO FROM NEW VERSION")
+``
